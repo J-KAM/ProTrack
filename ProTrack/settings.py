e5095eb634eaf61dbefe23c8296a3d0209b364bb
Jelena K <jelenakalabic@gmail.com>
2018-01-24 16:45:20 +0100

    Added login required decorators and base.html moved.

--- ProTrack/settings.py
+++ ProTrack/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'db',
-        #'HOST': 'localhost',
+        # 'HOST': 'localhost',
         'PORT': 5432,
     }
 }
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_FINDERS = ('django.contrib.staticfiles.finders.AppDirectoriesFinder',)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
