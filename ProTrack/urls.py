82882f3c922ddccf08abb78f08e8fc442334086c
Ana <1994.ana@gmail.com>
2018-01-26 18:55:45 +0100

    Implemented basic project creating. #14
    Project has a unique name, description, date of creation and owner (currently logged user).

--- ProTrack/urls.py
+++ ProTrack/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('core.urls')),
+    url(r'^projects/', include('projects.urls')),
 
 ]
 
