f67e992334feed0c5ade39a4e0ab8561018ad4a7
Jelena K <jelenakalabic@gmail.com>
2018-01-27 23:21:29 +0100

    Implemented organization preview. #16

--- organizations/urls.py
+++ organizations/urls.py
@@ -5,5 +5,6 @@ from organizations import views
 app_name = 'organizations'
 
 urlpatterns = [
+    url(r'^$', views.OrganizationPreview.as_view(), name="preview"),
     url(r'^new/$', views.OrganizationFormView.as_view(), name="create"),
 ]
