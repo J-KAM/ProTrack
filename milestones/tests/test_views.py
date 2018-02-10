f53b0f5242e7a12a1c02111da7022767cde2d474
Ana <1994.ana@gmail.com>
2018-02-10 15:20:53 +0100

    Removed unnecessary milestone view tests. #12

--- milestones/tests/test_views.py
+++ milestones/tests/test_views.py
@@ -150,61 +150,6 @@ class MilestoneDeleteTest(TestCase):
         self.assertTemplateUsed(response, 'milestones/milestone_confirm_delete.html')
 
 
-class MilestoneDetailTest(TestCase):
-    def test_urls_exist_at_desired_location(self):
-        login = self.client.login(username='pera', password='pera1234')
-        response = self.client.get('/milestones/' + str(self.MILE1_ID) + '/details/')
-
-        self.assertEqual(str(response.context['user']), 'pera')
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_accessible_by_urls(self):
-        login = self.client.login(username='pera', password='pera1234')
-
-        response = self.client.get(reverse('milestones:detail', kwargs={'id': self.MILE1_ID}))
-        self.assertEqual(response.status_code, 200)
-
-    def test_logged_in_uses_correct_template(self):
-        login = self.client.login(username='pera', password='pera1234')
-        response = self.client.get(reverse('milestones:detail', kwargs={'id': self.MILE1_ID}))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'milestones/detail_preview.html')
-
-    def setUp(self):
-        test_user1 = User.objects.create_user(username='pera', email='pera@gmail.com', password='pera1234',first_name='Pera', last_name='Peric')
-        self.USER1_ID = test_user1.id
-
-        test_project1 = Project.objects.create(name='First project', url='localhost:8000/pera/First project',
-                                               description='my first project', created='2018-02-01', num_of_stars=0,
-                                               owner=test_user1, organization_owner=None)
-
-        self.PRO1_ID = test_project1.id
-
-        milestone1 = Milestone.objects.create(name='Initial', description='my inital milestone',
-                                              start_date='2018-02-03', due_date='2018-02-07', total_progress=0,
-                                              total_time_spent=0.0, status='OPEN', project=test_project1)
-        self.MILE1_ID = milestone1.id
-
-    def test_urls_exist_at_desired_location(self):
-        login = self.client.login(username='pera', password='pera1234')
-        response = self.client.get('/milestones/' + str(self.MILE1_ID) + '/details/')
-
-        self.assertEqual(str(response.context['user']), 'pera')
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_accessible_by_urls(self):
-        login = self.client.login(username='pera', password='pera1234')
-
-        response = self.client.get(reverse('milestones:detail', kwargs={'id': self.MILE1_ID}))
-        self.assertEqual(response.status_code, 200)
-
-    def test_logged_in_uses_correct_template(self):
-        login = self.client.login(username='pera', password='pera1234')
-        response = self.client.get(reverse('milestones:detail', kwargs={'id': self.MILE1_ID}))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'milestones/detail_preview.html')
-
-
 class ViewMethodsTest(TestCase):
 
     def setUp(self):
