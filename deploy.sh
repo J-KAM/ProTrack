dea2b613954a5e0903f0b3fcc1e7b98b98c491ff
Jelena K <jelenakalabic@gmail.com>
2018-02-22 17:35:07 +0100

    Trying comands for migrations. #30

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker-compose down
 docker-compose pull
+docker-compose up -d
 docker-compose exec web python manage.py makemigrations
 docker-compose exec web python manage.py migrate
-docker-compose up -d
